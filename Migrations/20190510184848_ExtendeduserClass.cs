96851e90002e4726b6434601f723fb771a867d6f
Sgoswami3 <goswami.sudeep@gmail.com>
2019-05-11 00:22:56 +0530

    UserModelExtended

---
+++ Migrations/20190510184848_ExtendeduserClass.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class ExtendeduserClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "city",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "country",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dateofbirth",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "gender",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "interest",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "introduction",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "knownas",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "lastactive",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "lookingfor",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    url = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    dateAdded = table.Column<DateTime>(nullable: false),
+                    IsMain = table.Column<bool>(nullable: false),
+                    UserID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_UserID",
+                table: "Photos",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "city",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "country",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "dateofbirth",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "gender",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "interest",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "introduction",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "knownas",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "lastactive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "lookingfor",
+                table: "Users");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace DatingApp.API.Models
 {
     public class User
@@ -8,6 +11,18 @@ namespace DatingApp.API.Models
         public string username { get; set; }
         public byte[] passwordHash { get; set; }
         public byte[] passwordSalt { get; set; }
+        public string gender { get; set; }
+        public DateTime dateofbirth { get; set; }
+        public string knownas { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime lastactive { get; set; }
+        public string introduction { get; set; }
+        public string lookingfor { get; set; }
+        public string interest { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+
+        public ICollection<Photo> photos { get; set; }
 
     }
 }
